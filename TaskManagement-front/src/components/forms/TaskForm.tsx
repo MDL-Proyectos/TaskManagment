f75928539c2b20be774de454156bf9261446eabb
Matias Di Lella <100892403+MDL-Proyectos@users.noreply.github.com>
2024-12-05 09:47:54 -0300

    TaskForm - Llistado de Usuarios y Equipos
    Se agrega listado de usuarios y equipos en los campos relacionados a la tarea.

--- TaskManagement-front/src/components/forms/TaskForm.tsx
+++ TaskManagement-front/src/components/forms/TaskForm.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Button, DatePicker, Form, Input, message, Select } from 'antd';
 import TaskServices from '../../routes/TaskServices.tsx';
+import UserServices from '../../routes/UserServices.tsx';
 import dayjs from 'dayjs'; //esto me permite ajutar los formatos de fechas y corregirlos en el momento de visualizarlo
+import { UsuarioData } from '../User.tsx';
+import TeamService from '../../routes/TeamServices.tsx';
+import { TeamData } from '../Team.tsx';
 
 type SizeType = Parameters<typeof Form>[0]['size']; 
 
@@ -11,6 +15,8 @@ const TaskForm: React.FC = () => {
   const [componentSize, setComponentSize] = useState<SizeType | 'default'>('default');
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const [users, setUsers] = useState<UsuarioData[]>([]); // Estado para almacenar la lista de usuarios
+  const [teams, setTeams] = useState<TeamData[]>([]); // Estado para almacenar la listlista de usuarios
 
   const onFormLayoutChange = ({ size }: { size: SizeType }) => {
     setComponentSize(size);
@@ -30,7 +36,6 @@ const TaskForm: React.FC = () => {
           created_at: comment.created_at ? dayjs(comment.created_at) : null,
         })),
       };
-
       // Actualiza los campos del formulario con los datos recibidos
       form.setFieldsValue(mappedData);
     } catch (error) {
@@ -38,11 +43,34 @@ const TaskForm: React.FC = () => {
       message.error('Error al cargar los datos de la tarea.');
     }
   };
+      // Función para obtener los datos de los usuarios
+      const fetchUsers = async () => {
+        try {
+          const listUsers = await UserServices.getUsers(); 
+          setUsers(listUsers); 
+        } catch (error) {
+          console.error('Error al obtener la lista de usuarios:', error);
+          message.error('Error al cargar la lista de usuarios.');
+        }
+      };
 
+       // Función para obtener los datos de los equipos
+       const fetchTeams = async () => {
+        try {
+          const listTeams = await TeamService.getAllTeams(); 
+          setTeams(listTeams); 
+        } catch (error) {
+          console.error('Error al obtener la lista de equipos:', error);
+          message.error('Error al cargar la lista de equipos.');
+        }
+      };
+    
   // Ejecutar fetchTask cuando el componente carga
   useEffect(() => {
     if (idTask) {
       fetchTask();
+      fetchUsers();
+      fetchTeams();
     }
   }, [idTask, form]);
 
@@ -129,6 +157,35 @@ const TaskForm: React.FC = () => {
       </Form.Item>
 
       <Form.Item
+        label="Usuario Asignado"
+        name={['assigned_user', '_id']}
+        rules={[{ required: true, message: 'Por favor, selecciona un usuario' }]}
+        >
+        <Select placeholder="Selecciona un usuario">
+            {users.map((user) => (
+            <Select.Option key={user._id} value={user._id}>
+                {user.first_name} {user.last_name}
+            </Select.Option>
+            ))}
+        </Select>
+    </Form.Item>   
+
+    <Form.Item
+        label="Equipo"
+        name={['assigned_team', 'idTeam']}
+        rules={[{ required: true, message: 'Por favor, selecciona un Equipo' }]}
+        >
+        <Select placeholder="Selecciona un Equipo">
+            {teams.map((team) => (
+            <Select.Option key={team.idTeam} value={team.idTeam}>
+                {team.name} 
+            </Select.Option>
+            ))}
+        </Select>
+    </Form.Item>   
+
+
+      <Form.Item
         label="Proyecto"
         name="project"
         rules={[{ required: true, message: 'Por favor, ingresa el proyecto' }]}
@@ -142,6 +199,19 @@ const TaskForm: React.FC = () => {
       >
         <Input.TextArea placeholder="Observaciones" />
       </Form.Item>
+      <Form.Item
+        label="Autorizacion"
+        name={['authorized_by', '_id']}
+        rules={[{ required: true, message: 'Por favor, selecciona un usuario' }]}
+        >
+        <Select placeholder="Selecciona un usuario">
+            {users.map((user) => (
+            <Select.Option key={user._id} value={user._id}>
+                {user.first_name} {user.last_name}
+            </Select.Option>
+            ))}
+        </Select>
+    </Form.Item>
 
       <Form.List name="comments">
         {(fields, { add, remove }) => (
