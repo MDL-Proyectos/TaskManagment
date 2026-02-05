a172e734235896c98076e18e21f9cf2b077436ec
Matias Di Lella <matias.adilella@gmail.com>
2026-02-05 11:00:59 -0300

    BACK - Update migrate-mongo-config.js

--- TaskManagement-backend/migrate-mongo-config.js
+++ TaskManagement-backend/migrate-mongo-config.js
@@ -1,21 +1,31 @@
 import dotenv from 'dotenv'
 /* eslint-disable no-undef */
-const env_path = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env'
+const env_path = process.env.NODE_ENV ? process.env.NODE_ENV : '.env'
 
-dotenv.config({ path: env_path })
+//dotenv.config({ path: env_path })
+dotenv.config();
 
-const db_url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/'
-const db_name = process.env.MONGO_DB || 'test'
+const db_url = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/'
+const db_name = process.env.MONGO_DB || 'db_task'
 
 const config = {
   mongodb: {
     url: db_url + db_name,
-    options: {},
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
   },
   migrationsDir: 'migrations',
   changelogCollectionName: 'changelog',
   migrationFileExtension: '.js',
-  useFileHash: false,
+  moduleSystem: 'esm',
 }
+
+export const mongodb = config.mongodb;
+export const migrationsDir = config.migrationsDir;
+export const changelogCollectionName = config.changelogCollectionName;
+export const migrationFileExtension = config.migrationFileExtension;
+export const moduleSystem = config.moduleSystem;
 
 export default config
