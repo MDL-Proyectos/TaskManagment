aba76644aeb045fe10e5fb6c9541ab0060fea0a1
Matias Di Lella <matias.adilella@gmail.com>
2026-02-05 10:49:21 -0300

    FRONT - page Team
    Mejoramos el componente porque se detectó un error visual cuando son pocos cards.

--- TaskManagement-front/src/pages/Teams.tsx
+++ TaskManagement-front/src/pages/Teams.tsx
@@ -75,7 +75,7 @@ function Teams() {
 
  
   return (
-    <div style={{  width: '100%', height: '100%', padding: '60px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
+    <div style={{ width: '90%', height: '90%', padding: '20px' }}>
         {/* <Title level={2} style={{ marginBottom: 50 }}>
             Equipos
          </Title>*/}
